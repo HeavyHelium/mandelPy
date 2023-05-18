ceee2534a43f8bed2a577196109827352e88b91f
Diana Markova <34967260+HeavyHelium@users.noreply.github.com>
2023-05-18 16:24:55 +0300

    add test results

--- src/script_runner.sh
+++ src/script_runner.sh
@@ -2,20 +2,23 @@
 
 
 # Check if script to run is provided as an argument
-if [ -z "$1" ]; then
-  echo "Usage: $0 <script_to_run>"
+if [ -z "$1" ] || [ -z "$2" ]; then
+  echo "Usage: $0 <script_to_run> <result output dir>"
   exit 1
 fi
 
+
 script_to_run="$1"
+output_dir="$2"
 
-
-for ((i=1; i<=5; i++))
+for ((i=1; i<=5; i++));
 do
     echo "Running $script_to_run $i"
-    output_file="results${i}.txt"
+    output_file="$output_dir/results${i}.txt"
 
     bash "$script_to_run" > "$output_file"
+
+    echo "Saved results to $output_file"
 
     # lull
     sleep 10
